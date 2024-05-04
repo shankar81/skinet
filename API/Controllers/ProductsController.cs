170e410759da8c7dc1d17ae3070a109ba1b91f0f
shankar sawant <shankarsawant81@gmail.com>
2024-05-04 18:51:18 +0530

    initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Core.Entities;
+using Infra.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly StoreContext _context;
+
+    public ProductsController(StoreContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        return await _context.Products.FindAsync(id);
+    }
+}
---
+++ Infra/Data/StoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
